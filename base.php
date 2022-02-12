fbdbe4aa751c02f05937696f041a0097922d04e8
Achu07M <20pc09@psgtech.ac.in>
2022-02-13 00:05:08 +0530

    homepage 2 commit

---
+++ base.php
@@ -0,0 +1,227 @@
+
+<html>
+    <head>
+    <style>
+     body{
+    background-color: #100E0E;
+}
+.navbar{
+    position: absolute;
+    width: 1500px;
+    height: 67px;
+    left: 0px;
+    top: 0px;
+    background: #100E0E;
+}
+
+.hamburgericon{
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    left: 18px;
+    top: 19px;
+}
+
+.logopic{
+    position: absolute;
+    width: 110px;
+    height: 66px;
+    left: 100px;
+    top: 0px;
+    overflow: hidden;
+}
+
+.logoname{
+    position: absolute;
+    width: 305px;
+    height: 66px;
+    left: 210px;
+    top: 0px;
+}
+
+.homeicon{
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    left: 1250px;
+    top: 18px;
+}
+.likeicon{
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    left: 1350px;
+    top: 18px;
+    
+}
+.bagicon{
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    left: 1450px;
+    top: 18px;
+}
+   
+
+        .footer{
+    position: absolute;
+    width: 1550px;
+    height: 500px;
+    left: 0px;
+    top: 4450px;
+    background: #EE519E;
+}
+.footertext1{
+    position: absolute;
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 168.99%;
+    text-transform: capitalize;
+    color: #FFFFFF;
+    height:233px;
+    width: 307px;
+    left:250px;
+    top:75px;
+}
+
+.footertext2{
+    position: absolute;
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 168.99%;
+    text-transform: capitalize;
+    color: #FFFFFF;
+    height:233px;
+    width: 400px;
+    left: 1125px;
+    top:75px;
+ }
+ .socialmediaicons p{
+    position: absolute;
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 168.99%;
+    text-transform: capitalize;
+    color: #FFFFFF;
+    height:233px;
+    width: 400px;
+    left: 650px;
+    top:75px
+ }
+
+#fbicon{
+    position:absolute;
+    width: 50px;
+    height: 50px;
+    left: 580px;
+    top: 150px;
+}
+#twittericon{
+    position:absolute;
+    width: 50px;
+    height: 50px;
+    left: 650px;
+    top: 150px;
+}
+#instaicon{
+    position:absolute;
+    width: 50px;
+    height: 50px;
+    left: 720px;
+    top: 150px;
+}
+#yticon{
+    position:absolute;
+    width: 50px;
+    height: 50px;
+    left: 790px;
+    top: 150px;
+}
+#whatsappicon{
+    position:absolute;
+    width: 50px;
+    height: 50px;
+    left: 860px;
+    top: 150px;
+}
+
+.paymentgateways p{
+    position: absolute;
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 168.99%;
+    text-transform: capitalize;
+    color: #FFFFFF;
+    height:233px;
+    width: 400px;
+    left: 650px;
+    top:255px
+}
+#cards{
+    position: absolute;
+width: 533px;
+height: 27px;
+top:350px;
+left:485px;
+
+}
+
+    </style>
+</head>
+    <body>
+        <div class="navbar">
+            <div class="hamburgericon">
+                <img src="./pictures/hamburgericon.png" alt="Menuicon">
+            </div>
+            <div class="logopic">
+                <img src="./pictures/logo.png" alt="Logo">
+            </div>
+            <div class="logoname">
+                <img src="./pictures/logoname.png" alt="Logo">
+            </div>
+            <div class="homeicon">
+                <img src="./pictures/homeicon.png" alt="Home">
+            </div >
+            <div class="likeicon">
+                <img src="./pictures/likeicon.png" alt="Home">
+            </div>
+            <div class="bagicon">
+                <img src="./pictures/bagicon.png" alt="Home">
+            </div>
+        </div>
+        <div class="footer">
+            <div class="footertext1">
+                <p id="contactus">Contact Us</p>
+                <p id="termsandconditions">Terms and Conditions</p>
+                <p id="termsofservice">Terms of service</p>
+                <p id="refundpolicy">Refund Policy</p>
+            </div>
+            <div class="footertext2">
+                <p id="privacypolicy">Privacy Policy</p>
+                <p id="shippingpolicy">Shipping Policy</p>
+                <p id="exchangepolicy">Exchange Policy</p>
+            </div>
+            <div class="socialmediaicons">
+                <p>Connect With Us</p>
+                <img id="fbicon" src="./pictures/facebook.png" alt="whatsapp">
+                <img id="twittericon" src="./pictures/twitter.png" alt="twitter">
+                <img id="instaicon" src="./pictures/instagram.png" alt="instagram">
+                <img id="yticon" src="./pictures/youtube.png" alt="youtube">
+                <img id="whatsappicon" src="./pictures/whatsapp.png" alt="youtube">
+            </div>
+            <div class="paymentgateways">
+                <p>Payment Gateways</p>
+                <img id="cards" src="./pictures/cards.png" alt="cards">
+            </div>
+        </div>
+    </body>
+</html>
+    
---
+++ home.php
@@ -0,0 +1,745 @@
+<?php
+ include "base.php";
+?>
+<head>
+    <title>Home</title>
+    <style>
+        .img1{
+    position: absolute;
+    left: 35px;
+    width: 1500px;
+    height: 429px;
+    top: 67px;
+}
+
+.text{
+    position: absolute;
+    width: 307px;
+    height: 57px;
+    left: 595px;
+    top: 475px;
+    font-family: Abhaya Libre;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 42px;
+    text-align: center;
+    color: #FFFFFF;
+}
+
+.cat_name#first{
+    width: 206px;
+    height: 43px;
+    left: 304px;
+    top: 750px;
+    position: absolute;
+    font-family: Abhaya Libre;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 33px;
+    text-align: center;
+    color: #FFFFFF;
+
+}
+.cat_name#second{
+    position: absolute;
+    font-family: Abhaya Libre;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 33px;
+    text-align: center;
+    color: #FFFFFF;
+    width: 206px;
+    height: 43px;
+    left: 519px;
+    top: 750px;
+}
+.cat_name#third{
+    position: absolute;
+    font-family: Abhaya Libre;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 33px;
+    text-align: center;
+    color: #FFFFFF;
+    width: 206px;
+    height: 43px;
+    left: 754px;
+    top: 750px;
+}
+.cat_name#fourth{
+    position: absolute;
+    font-family: Abhaya Libre;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 33px;
+    text-align: center;
+    color: #FFFFFF;
+    width: 206px;
+    height: 43px;
+    left: 985px;
+    top: 750px;
+}
+.vector{
+    position: absolute;
+    width: 261.11px;
+    height: 379px;
+    left: 1000px;
+    top: 115px;
+}
+.textframe1{
+    position: absolute;
+    width: 448.18px;
+    height: 92px;
+    left: 360.52px;
+    top: 78px;
+    font-family: Phetsarath;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 72px;
+    line-height: 105px;
+    text-align: center;
+    letter-spacing: 0.045em;
+    color: #E5E5E5;
+}
+.textframe2{
+    position: absolute;
+    width: 719px;
+    height: 86px;
+    left: 247px;
+    top: 200px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 64px;
+    line-height: 75px;
+    text-align: center;
+    color: #FFECEC;
+}
+.art{
+    position: absolute;
+    width: 146px;
+    height: 534.89px;
+    left: 35.89px;
+    top: 350px;
+}
+.modelpic{
+    position: absolute;
+    width: 307.88px;
+    height: 429px;
+    left: 1000px;
+    top: 68px;
+}
+.rectangle11{
+    position: absolute;
+    width: 408px;
+    height: 190px;
+    left: 97.5px;
+    top: 905.5px;
+    border: 5px solid #FFFFFF;
+    box-sizing: border-box;
+    filter: blur(0.758333px);
+ }
+ .rectangle12{
+    position: absolute;
+    width: 408px;
+    height: 190px;
+    left: 525px;
+    top: 905.5px;
+    border: 5px solid #FFFFFF;
+    box-sizing: border-box;
+    filter: blur(0.758333px);
+ }
+ .rectangle13{
+    position: absolute;
+    width: 408px;
+    height: 190px;
+    left: 950px;
+    top: 905.5px;
+    border: 5px solid #FFFFFF;
+    box-sizing: border-box;
+    filter: blur(0.758333px);
+ }
+ .rectangle21{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 101.5px;
+    top: 909.5px;
+    border: 4.55px solid #FB37FF;
+    box-sizing: border-box;
+    filter: blur(1.1375px);
+}
+.rectangle22{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 528px;
+    top: 909.5px;
+    border: 4.55px solid #FB37FF;
+    box-sizing: border-box;
+    filter: blur(1.1375px);
+}
+.rectangle23{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 955px;
+    top: 909.5px;
+    border: 4.55px solid #FB37FF;
+    box-sizing: border-box;
+    filter: blur(1.1375px);
+}
+.rectangle31{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 101.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 7.39375px solid rgba(251, 55, 255, 0.2);
+    box-sizing: border-box;
+    box-shadow: inset -0.739375px 1.47875px 0.739375px rgba(255, 255, 255, 0.5);
+    filter: drop-shadow(0px 11.375px 14.7875px #990099) drop-shadow(0px 7.39375px 5.6875px rgba(77, 0, 66, 0.7));
+}
+.rectangle32{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 530.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 7.39375px solid rgba(251, 55, 255, 0.2);
+    box-sizing: border-box;
+    box-shadow: inset -0.739375px 1.47875px 0.739375px rgba(255, 255, 255, 0.5);
+    filter: drop-shadow(0px 11.375px 14.7875px #990099) drop-shadow(0px 7.39375px 5.6875px rgba(77, 0, 66, 0.7));
+}
+.rectangle33{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 957.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 7.39375px solid rgba(251, 55, 255, 0.2);
+    box-sizing: border-box;
+    box-shadow: inset -0.739375px 1.47875px 0.739375px rgba(255, 255, 255, 0.5);
+    filter: drop-shadow(0px 11.375px 14.7875px #990099) drop-shadow(0px 7.39375px 5.6875px rgba(77, 0, 66, 0.7));
+}
+.rectangle41{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 101.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 9.66875px solid #FF26C2;
+    box-sizing: border-box;
+    filter: blur(11.375px);
+}
+
+.rectangle42{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 530.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 9.66875px solid #FF26C2;
+    box-sizing: border-box;
+    filter: blur(11.375px);
+}
+.rectangle43{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 957.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 9.66875px solid #FF26C2;
+    box-sizing: border-box;
+    filter: blur(11.375px);
+}
+.rectangle51{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 101.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 7.39375px solid #F32E7C;
+    box-sizing: border-box;
+    filter: blur(34.125px);
+}
+.rectangle52{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 530.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 7.39375px solid #F32E7C;
+    box-sizing: border-box;
+    filter: blur(34.125px);
+}
+.rectangle53{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 957.5px;
+    top: 909.5px;
+    mix-blend-mode: hard-light;
+    border: 7.39375px solid #F32E7C;
+    box-sizing: border-box;
+    filter: blur(34.125px);
+}
+.ellipse11{
+    position: absolute;
+    width: 462px;
+    height: 193.2px;
+    left: 60px;
+    top: 893.4px;
+    background: rgba(255, 55, 128, 0.2);
+    mix-blend-mode: hard-light;
+    filter: blur(34.125px);
+}
+.ellipse12{
+    position: absolute;
+    width: 462px;
+    height: 193.2px;
+    left: 525px;
+    top: 893.4px;
+    background: rgba(255, 55, 128, 0.2);
+    mix-blend-mode: hard-light;
+    filter: blur(34.125px);
+}
+.ellipse13{
+    position: absolute;
+    width: 462px;
+    height: 193.2px;
+    left: 950px;
+    top: 893.4px;
+    background: rgba(255, 55, 128, 0.2);
+    mix-blend-mode: hard-light;
+    filter: blur(34.125px);
+}
+.rectangle61{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 102px;
+    top: 908px;
+    background: rgba(238, 81, 158, 0.51);
+}
+.rectangle62{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 525px;
+    top: 908px;
+    background: rgba(238, 81, 158, 0.51);
+}
+.rectangle63{
+    position: absolute;
+    width: 400px;
+    height: 181px;
+    left: 950px;
+    top: 908px;
+    background: rgba(238, 81, 158, 0.51);
+}
+.splash1{
+    position: absolute;
+    width: 25px;
+    height: 400px;
+    left: 108px;
+    top: 918px;
+}
+.splash2{
+    position: absolute;
+    width: 25px;
+    height: 400px;
+    left: 535px;
+    top: 918px;
+}
+.splash3{
+    position: absolute;
+    width: 25px;
+    height: 400px;
+    left: 965px;
+    top: 918px;
+}
+.collectionheading{
+    position: absolute;
+    width: 226px;
+    height: 31px;
+    left: 626px;
+    top: 1135px;
+    font-family: Abhaya Libre;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 42px;
+    text-align: center;
+    color: #FFFFFF;
+}
+.txt1{
+    position: absolute;
+    width: 221px;
+    height: 49px;
+    left: 184px;
+    top: 900px;
+    font-family: Rokkitt;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 55px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+.txt2{
+    position: absolute;
+    width: 221px;
+    height: 49px;
+    left: 610px;
+    top: 870px;
+    font-family: Rokkitt;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 55px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+.txt3{
+    position: absolute;
+    width: 221px;
+    height: 49px;
+    left: 1030px;
+    top: 880px;
+    font-family: Rokkitt;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 55px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+
+.txt11{
+    position: absolute;
+    width: 359px;
+    height: 34px;
+    left: 109px;
+    top: 985px;
+    font-family: Coda;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 51px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+.txt12{
+    position: absolute;
+    width: 359px;
+    height: 34px;
+    left: 553px;
+    top: 985px;
+    font-family: Coda;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 51px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+.txt13{
+    position: absolute;
+    width: 359px;
+    height: 34px;
+    left: 975px;
+    top: 985px;
+    font-family: Coda;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 51px;
+    text-align: center;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+.bgrectangle{
+    position: absolute;
+    width: 1550px;
+    height: 771px;
+    left: 0px;
+    top: 1223px;
+
+background: #292D33;
+}
+.collectionrectangle1{
+    position: absolute;
+    width: 698px;
+    height: 469px;
+    left: 20px;
+    top: 1242px;
+
+background: #DA1D6F;
+}
+.collection1img{
+    position: absolute;
+    width: 485px;
+    height: 508px;
+    left: 233px;
+    top: 1223px;
+}
+.collection1name{
+    position: absolute;
+    width: 523px;
+    height: 181px;
+    left: -193px;
+    top: 1375px;
+    font-family: Impact;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 55px;
+    line-height: 98px;
+    text-align: center;
+    letter-spacing: 0.295em;
+    text-transform: uppercase;
+    color: #FFFFFF;
+    transform: rotate(-90deg);
+}
+.casuals{
+    position: absolute;
+    width: 240px;
+    height: 678px;
+    left: 0px;
+    top: 1700px;
+}
+.greyshadow{
+    width: 240px;
+    height: 678px;
+    left: 0px;
+    top: 1780px;
+}
+.casualimg1{
+    position: absolute;
+    width: 256px;
+    height: 246px;
+    left: 100px;
+    top: 1715px;
+}
+.casualimg2{
+    position: absolute;
+    width: 206px;
+    height: 221px;
+    left: 274px;
+    top: 1740px;
+}
+.casualimg3{
+    position: absolute;
+    width: 268px;
+    height: 228px;
+    left: 394px;
+    top: 1733px;
+ }
+ .round1{
+    border-radius: 50%;
+    position: absolute;
+    width: 183px;
+    height: 183px;
+    left: 312px;
+    top: 585px;
+    background: #DA1D6F;
+}
+.roundimg1{
+    position: absolute;
+    border-radius: 50%;
+    overflow: hidden;
+    left: 328px;
+    top: 600px;
+}
+.round2{
+    border-radius: 50%;
+    position: absolute;
+    width: 183px;
+    height: 183px;
+    left: 535px;
+    top: 585px;
+    background: #DA1D6F;
+}
+.roundimg2{
+    position: absolute;
+    border-radius: 50%;
+    overflow: hidden;
+    left: 551px;
+    top: 600px;
+
+}
+.round3{
+    border-radius: 50%;
+    position: absolute;
+    width: 183px;
+    height: 183px;
+    left: 766px;
+    top: 585px;
+    background: #DA1D6F;
+}
+.roundimg3{
+    position: absolute;
+    border-radius: 50%;
+    overflow: hidden;
+    left: 782px;
+    top: 600px;
+
+}
+.round4{
+    border-radius: 50%;
+    position: absolute;
+    width: 183px;
+    height: 183px;
+    left: 997px;
+    top: 585px;
+    background: #DA1D6F;
+}
+.roundimg4{
+    position: absolute;
+    border-radius: 50%;
+    overflow: hidden;
+    left: 1013px;
+    top: 600px;
+
+}
+    </style>
+</head>
+<body>
+        <div class="frame1">
+            <div class="img1">
+                <img src="./pictures/add1.png" alt="Ad">
+            </div>
+            <div class="textframe1">
+                <p>Fashion</p>
+            </div>
+            <div class="textframe2">
+                <p>For Your Best Moments</p>
+            </div>
+            <div class="art">
+                <img src="./pictures/flower.png" alt="art">
+            </div>
+            <div class="vector">
+                <img src="./pictures/vector.png" alt="Ad">
+            </div>
+            <div class="modelpic">
+                <img src="./pictures/addpic.png" alt="Ad">
+            </div>
+        </div>
+        <div class="frame2">
+            <div class="text">
+                <p>Top Categories</p>
+            </div>
+                <div class="round1"></div>
+                <div class="roundimg1">
+                    <img src="./pictures/round1.png" alt="r1">
+                </div>
+                <div id="first" class="cat_name" >
+                    <p>Saree</p>
+                </div>
+                <div class="round2"></div>
+                <div class="roundimg2">
+                    <img src="./pictures/round2.png" alt="r1">
+                </div>
+                <div id="second"  class="cat_name">
+                    <p>Salwars</p>
+                </div>
+                <div class="round3"></div>
+                <div class="roundimg3">
+                    <img src="./pictures/round3.png" alt="r1">
+                </div>
+                <div id="third"  class="cat_name">
+                    <p>kurtis</p>
+                </div>
+                <div class="round4"></div>
+                <div class="roundimg4">
+                    <img src="./pictures/round4.png" alt="r1">
+                </div>
+                <div id="fourth"  class="cat_name">
+                    <p>Shawls</p>
+                </div>
+        </div>
+        <div class="sale">
+            <div class="rectangle11" id="r11"></div>
+            <div class="rectangle21"></div>
+            <div class="rectangle31"></div>
+            <div class="rectangle41"></div>
+            <div class="rectangle51"></div>
+            <div class="ellipse11"></div>
+            <div class="splash1">
+                <img src="./pictures/unspash.png" alt="splash">
+            </div>
+            <div class="rectangle61"></div>
+            <div class="txt1"><p>40% oFF</p></div>
+            <div class="txt11"><p>Amazing deals here</p></div>
+
+            <div class="rectangle12" ></div>
+            <div class="rectangle22"></div>
+            <div class="rectangle32"></div>
+            <div class="rectangle42"></div>
+            <div class="rectangle52"></div>
+            <div class="ellipse12"></div>
+            <div class="splash2">
+                <img src="./pictures/unspash.png" alt="splash">
+            </div>
+            <div class="rectangle62"></div>
+            <div class="txt2"><p>FREE SHIPPING</p></div>
+            <div class="txt12"><p>For orders above 2500</p></div>
+
+            <div class="rectangle13" ></div>
+            <div class="rectangle23"></div>
+            <div class="rectangle33"></div>
+            <div class="rectangle43"></div>
+            <div class="rectangle53"></div>
+            <div class="ellipse13"></div>
+            <div class="splash3">
+                <img src="./pictures/unspash.png" alt="splash">
+            </div>
+            <div class="rectangle63"></div>
+            <div class="txt3"><p>SELECTED      STOCK</p></div>
+            <div class="txt13"><p>Buy 1 get 1 free</p></div>
+
+        </div>
+        <div class="collection">
+            <div class="collectionheading"><p>Collections</p></div>
+            <div class="bgrectangle"></div>
+            <div class="collectionrectangle1"></div>
+            <div class="collection1img">
+                <img src="./pictures/collection1.png" alt="img">
+            </div>
+            <div class="collection1name"><p>LATEST COLLECTIONS</p></div>
+            <div class="casuals">
+                <img src="./pictures/casuals background.png" alt="bg">
+            </div>
+            <div class="greyshadow">
+                <img src="./pictures/grey shadow cottons.png" alt="shadow">
+            </div>
+            <div class="casualimg1">
+                <img src="./pictures/model1.png" alt="shadow">
+            </div>
+            <div class="casualimg2">
+                <img src="./pictures/model2.png" alt="shadow">
+            </div>
+            <div class="casualimg3">
+                <img src="./pictures/model3.png" alt="shadow">
+            </div>
+        </div>
+    </body>
+       
